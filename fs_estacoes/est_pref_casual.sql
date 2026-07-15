749d8ce0d88dc380fb92101704197f5229db78c3
Fernanda Moura <ferferreira199609@gmail.com>
2026-07-14 22:00:47 -0300

    fs_estações

---
+++ fs_estacoes/est_pref_casual.sql
@@ -0,0 +1,14 @@
+SELECT
+a.start_station_id,
+b.station_name,
+     COUNT(a.start_station_id) AS preferidas_member
+
+FROM trips as a
+
+LEFT JOIN dim_station as b
+ON a.start_station_id = b.station_id 
+
+WHERE a.member_casual = 'casual'
+GROUP BY a.start_station_id, b.station_name
+ORDER BY preferidas_member DESC
+LIMIT 100
